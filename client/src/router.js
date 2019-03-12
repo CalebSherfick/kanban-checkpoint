4df9334519c1104ef371dfafac9a3d0ca4e80aab
Brett Wilcox <wilcox.brett@gmail.com>
2019-03-12 12:17:39 -0600

    12:17

--- client/src/router.js
+++ client/src/router.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import Boards from './views/Boards.vue'
 import Board from './views/Board.vue'
 import Login from './views/Login.vue'
-import Lists from './views/Lists.vue'
+// import Lists from './views/Lists.vue'
 
 Vue.use(Router)
 
@@ -29,10 +29,10 @@ export default new Router({
       path: "*",
       redirect: '/'
     },
-    {
-      path: '/board/:boardId/lists',
-      name: 'lists',
-      component: Lists
-    },
+    // {
+    //   path: '/board/:boardId/lists',
+    //   name: 'lists',
+    //   component: Lists
+    // },
   ]
 })
