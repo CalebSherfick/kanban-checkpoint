e220c944cb93fab9b78b8356601812854c77f3f3
Brett Wilcox <wilcox.brett@gmail.com>
2019-03-12 12:08:53 -0600

    12:08

--- client/src/router.js
+++ client/src/router.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import Boards from './views/Boards.vue'
 import Board from './views/Board.vue'
 import Login from './views/Login.vue'
-import Lists from './views/List.vue'
+import Lists from './views/Lists.vue'
 
 Vue.use(Router)
 
@@ -30,7 +30,7 @@ export default new Router({
       redirect: '/'
     },
     {
-      path: '/lists',
+      path: '/board/:boardId/lists',
       name: 'lists',
       component: Lists
     },
